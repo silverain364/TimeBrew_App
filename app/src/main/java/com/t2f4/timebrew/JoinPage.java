be19bf76a1968c38b45410612784c0c27c4b19c0
yuhaSon <skt0309@naver.com>
2024-10-30 13:22:03 +0900

    feat. 로그인/인트로/가입 페이지 클래스 생성

---
+++ app/src/main/java/com/t2f4/timebrew/JoinPage.java
@@ -0,0 +1,14 @@
+package org.techtown.timebrew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_page);
+    }
+}
