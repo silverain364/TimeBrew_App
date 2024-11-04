16044e885c113056961c69e961f2ce291c7dc30f
KJW <cmcncb13624@gmail.com>
2024-11-04 21:01:01 +0900

    feat. test 환경 구축(JS)

--- app/src/main/java/com/t2f4/timebrew/LoginPage.java
+++ app/src/main/java/com/t2f4/timebrew/LoginPage.java
@@ -3,6 +3,7 @@ package com.t2f4.timebrew;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,13 @@ public class LoginPage extends AppCompatActivity {
         setContentView(R.layout.login_page);
 
         TextView signUp_Txt = findViewById(R.id.SignUp_txt);
+        Button signInBtn = findViewById(R.id.Login_btn);
+
+        signInBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginPage.this, table_settings.class);
+            startActivity(intent);
+        });
+
         signUp_Txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
