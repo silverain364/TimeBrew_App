016d17564d81b8740bbd2feda0314a3970c58695
yuhaSon <skt0309@naver.com>
2024-11-28 12:09:27 +0900

    feat. 관리자 정보 페이지 수정

--- app/src/main/java/com/t2f4/timebrew/TemplatePage.java
+++ app/src/main/java/com/t2f4/timebrew/TemplatePage.java
@@ -54,13 +54,13 @@ public class TemplatePage extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId() == R.id.nav_member_info_page) {
                     fragmentManager.beginTransaction()
-                            .replace(R.id.frameLayoutContainer, memberInfoPage) // fragment_container는 Fragment를 표시할 레이아웃 ID입니다.
-                            .addToBackStack(null) // 뒤로 가기 버튼을 누르면 이전 Fragment로 돌아가도록 설정
+                            .replace(R.id.frameLayoutContainer, memberInfoPage)
+                            .addToBackStack(null)
                             .commit();
                 } else if (item.getItemId() == R.id.nav_table_settings_page) {
                     fragmentManager.beginTransaction()
-                            .replace(R.id.frameLayoutContainer, tableSettingFragment) // fragment_container는 Fragment를 표시할 레이아웃 ID입니다.
-                            .addToBackStack(null) // 뒤로 가기 버튼을 누르면 이전 Fragment로 돌아가도록 설정
+                            .replace(R.id.frameLayoutContainer, tableSettingFragment)
+                            .addToBackStack(null)
                             .commit();
                 } else if (item.getItemId() == R.id.nav_arduino_page) {
                     fragmentManager.beginTransaction()
