9dc919b0dc4a8f5c8aef72e2f99a4b7df8969cd6
yuhaSon <skt0309@naver.com>
2024-12-02 11:51:54 +0900

    feat. 진동벨 관리 페이지 xml 구현

---
+++ app/src/main/java/com/t2f4/timebrew/VibratingBellPage.java
@@ -0,0 +1,19 @@
+package com.t2f4.timebrew;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class VibratingBellPage extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.vibratingbell_page, container, false);
+
+        return root;
+    }
+}
